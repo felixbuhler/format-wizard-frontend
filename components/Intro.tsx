b656b18a541aefd1e5ac875e36b0ec41eed85e76
felixbuhler <felix.buhler@me.com>
2022-10-10 16:50:19 +0200

    Update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Intro() {
     return (
         <section id="intro">
-            <div class="intro-text">
-                <span className='bd'>FORMAT WIZARD</span> is a collection of <span className='serif'>common used formats</span> for designers.
-                You'll
+            <div className="intro-text">
+                <span className="bd">FORMAT WIZARD</span> is a collection of <span className="serif">common used formats</span> for designers.
+                You&quot;ll
                 find print formats for DIN
-                sizes and international sizes. Also you'll find digital formats for TikTok, Instagram and many more. 🔮 
+                sizes and international sizes. Also you&quot;ll find digital formats for TikTok, Instagram and many more.
             </div>
         </section>
     )
